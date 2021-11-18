fa6628d92c5702438693f44a44c6b838e392f426
FetchTheMoon <79921141+FetchTheMoon@users.noreply.github.com>
2021-11-18 10:44:52 +0800

    给折叠版块大图增加切换按钮

--- src/tasks/hide-forum-rule-image.ts
+++ src/tasks/hide-forum-rule-image.ts
@@ -1,26 +1,41 @@
 import { getItem } from '@/utilities/storage';
 import { Selector } from '@/utilities/forum';
 import $ from 'jquery';
-import { extract } from '@/utilities/misc';
+import { addStyle, extract } from '@/utilities/misc';
+import CSS from '@/css/hide-forum-rule-image.css';
+
 
 let hideForumRuleImage: boolean | undefined;
-
 export default async function TASK_HideForumRuleImage(item: HTMLElement) {
-
     if (!document.URL.includes('/thread.php') && !document.URL.includes('/thread_new.php')) return;
-
     hideForumRuleImage = hideForumRuleImage ?? await getItem('Switch::hide-forum-rule-image');
     if (!hideForumRuleImage) return;
     if (!item.closest(Selector.RULE)) return;
+    if ($('#spp-hide-forum-rule-image-button').length) return;
+
+    addStyle(CSS, 'hide-forum-rule-image');
+
     const fid = Number(extract(document.URL, /fid-(\d+)/));
     const whiteList = [
         9,      // 茶馆 - 以和为贵, 以礼待人
         135,    // GALGAME汉化区 - 虚拟抱抱
     ];
     // console.info(`开始隐藏版块大图`);
-    if (!whiteList.includes(fid)) {
-        $(Selector.RULE_IMAGE_MEDIUM).removeAttr('src');
-    }
+    const $mediumImg = whiteList.includes(fid) ? undefined : $(Selector.RULE_IMAGE_MEDIUM);
+    const $bigImg = $(Selector.RULE_IMAGE_BIG);
+    $mediumImg?.addClass('spp-hide');
+    $bigImg?.addClass('spp-hide');
+    const $switchButton = $(`<button id="spp-hide-forum-rule-image-button">查看版块大图</button>`);
 
-    $(Selector.RULE_IMAGE_BIG).removeAttr('src');
+    ($bigImg
+        ? $bigImg
+        : $mediumImg
+            ? $mediumImg
+            : undefined)?.before($switchButton);
+
+    $switchButton.on('click', (e) => {
+        $mediumImg?.toggleClass('spp-hide');
+        $bigImg?.toggleClass('spp-hide');
+    });
+
 }
